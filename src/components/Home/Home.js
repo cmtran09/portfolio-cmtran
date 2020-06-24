3274081448585d76f0a05502570d841e94094b1e
cmtran09 <cmtran@hotmail.co.uk>
2020-06-24 21:54:39 +0100

    bulma added and initla contnet added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,31 @@
 import React from 'react'
 
+import 'bulma/css/bulma.css'
+// import '../styles/styles.scss'
+
 export default function Home() {
   return (
-    <div>Hello home
-    </div>
+    <section className="section">
+      <div className="container">
+        <div className="container">
+          <div className="level header">
+            <p className="level-left">Portfolio</p>
+            <p className="level-item">2020</p>
+            <p className="level-right">London</p>
+          </div>
+        </div>
+        <h1 className="name">
+          Cuong Tran
+        </h1>
+        <p className="title">
+          I am a Junior Software Engineer from London, recently graduated from General Assembly. Here are pieces of my work up to this point. I hope you enjoy having a look.
+        </p>
+        <ul>
+          <li>GitHub</li>
+          <li>LinkedIn</li>
+          <li>CodeWars</li>
+        </ul>
+      </div>
+    </section>
   )
 }
