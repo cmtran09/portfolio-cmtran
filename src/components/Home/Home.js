7122446b390a0beb85f402763b1ee117f37c1b9d
cmtran09 <cmtran@hotmail.co.uk>
2020-06-28 20:47:41 +0100

    skills name onhover change added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -17,7 +17,7 @@ export default function Home() {
   return (
     <main>
       <div className="skill-namer">
-        <p className="skill-text">{skill ? skill : "SKILLNAME"}</p>
+        <p className="skill-text">{skill ? skill : ""}</p>
       </div>
       <section className="section">
         <div className="container main-wrapper">
@@ -52,41 +52,41 @@ export default function Home() {
         <div className="container skills-wrapper">
           {/* <button onClick={e=>console.log(skill)}>click</button> */}
           <ul className="skills">
-            <span className="icon"><img onClick={e => currentSkill(e)} skillname="JavaScript" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/javascript.svg" /></span>
-            <span className="icon"><img onClick={e => currentSkill(e)} skillname="HTML5" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/html5.svg" /></span>
-            <span className="icon"><img onClick={e => currentSkill(e)} skillname="html" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/html5.svg" /></span>
-            <span className="icon"><img onClick={e => currentSkill(e)} skillname="CSS3" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/css3.svg" /></span>
-            <span className="icon"><img onClick={e => currentSkill(e)} skillname="Node.js" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/node-dot-js.svg" /></span>
-            <span className="icon"><img onClick={e => currentSkill(e)} skillname="Express.js" src="https://www.vectorlogo.zone/logos/expressjs/expressjs-icon.svg"></img></span>
-            <span className="icon"><img onClick={e => currentSkill(e)} skillname="Python" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/python.svg" /></span>
-            <span className="icon"><img onClick={e => currentSkill(e)} skillname="Django" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/django.svg" /></span>
-            <span className="icon"><img onClick={e => currentSkill(e)} skillname="PostgreSQL" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/postgresql.svg" /></span>
-            <span className="icon"><img onClick={e => currentSkill(e)} skillname="Heroku" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/heroku.svg" /></span>
-            <span className="icon"><img onClick={e => currentSkill(e)} skillname="SQLite" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/sqlite.svg" /></span>
-            <span className="icon"><img onClick={e => currentSkill(e)} skillname="MongoDB" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/mongodb.svg" /></span>
-            <span className="icon"><img onClick={e => currentSkill(e)} skillname="Git" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/git.svg" /></span>
-            <span className="icon"><img onClick={e => currentSkill(e)} skillname="GitHub" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/github.svg" /></span>
-            <span className="icon"><img onClick={e => currentSkill(e)} skillname="Twilio" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/twilio.svg" /></span>
-            <span className="icon"><img onClick={e => currentSkill(e)} skillname="Sass" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/sass.svg" /></span>
-            <span className="icon"><img onClick={e => currentSkill(e)} skillname="Bulma" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/bulma.svg" /></span>
-            <span className="icon"><img onClick={e => currentSkill(e)} skillname="Material-UI" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/material-ui.svg" /></span>
-            <span className="icon"><img onClick={e => currentSkill(e)} skillname="Webpack" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/webpack.svg" /></span>
-            <span className="icon"><img onClick={e => currentSkill(e)} skillname="Babel" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/babel.svg" /></span>
-            <span className="icon"><img onClick={e => currentSkill(e)} skillname="Bootstrap" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/bootstrap.svg" /></span>
-            <span className="icon"><img onClick={e => currentSkill(e)} skillname="Recharts/D3.js" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/d3-dot-js.svg" /></span>
-            <span className="icon"><img onClick={e => currentSkill(e)} skillname="Redux" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/redux.svg" /></span>
-            <span className="icon"><img onClick={e => currentSkill(e)} skillname="Apollo" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/apollographql.svg" /></span>
-            <span className="icon"><img onClick={e => currentSkill(e)} skillname="GraphQL" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/graphql.svg" /></span>
-            <span className="icon"><img onClick={e => currentSkill(e)} skillname="npm" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/npm.svg" /></span>
-            <span className="icon"><img onClick={e => currentSkill(e)} skillname="Yarn" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/yarn.svg" /></span>
-            <span className="icon"><img onClick={e => currentSkill(e)} skillname="Mocha" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/mocha.svg" /></span>
-            <span className="icon"><img onClick={e => currentSkill(e)} skillname="Jest" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/jest.svg" /></span>
-            <span className="icon"><img onClick={e => currentSkill(e)} skillname="Socket.io" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/socket-dot-io.svg" /></span>
-            <span className="icon"><img onClick={e => currentSkill(e)} skillname="VSCode" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/visualstudiocode.svg" /></span>
-            <span className="icon"><img onClick={e => currentSkill(e)} skillname="Postman" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/postman.svg" /></span>
-            <span className="icon"><img onClick={e => currentSkill(e)} skillname="Insomnia" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/insomnia.svg" /></span>
-            <span className="icon"><img onClick={e => currentSkill(e)} skillname="StackOverflow" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/stackoverflow.svg" /></span>
-            <span className="icon"><img onClick={e => currentSkill(e)} skillname="Figma" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/figma.svg" /></span>
+            <span className="icon"><img onMouseEnter={e => currentSkill(e)} skillname="JavaScript" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/javascript.svg" /></span>
+            <span className="icon"><img onMouseEnter={e => currentSkill(e)} skillname="HTML5" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/html5.svg" /></span>
+            <span className="icon"><img onMouseEnter={e => currentSkill(e)} skillname="html" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/html5.svg" /></span>
+            <span className="icon"><img onMouseEnter={e => currentSkill(e)} skillname="CSS3" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/css3.svg" /></span>
+            <span className="icon"><img onMouseEnter={e => currentSkill(e)} skillname="Node.js" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/node-dot-js.svg" /></span>
+            <span className="icon"><img onMouseEnter={e => currentSkill(e)} skillname="Express.js" src="https://www.vectorlogo.zone/logos/expressjs/expressjs-icon.svg"></img></span>
+            <span className="icon"><img onMouseEnter={e => currentSkill(e)} skillname="Python" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/python.svg" /></span>
+            <span className="icon"><img onMouseEnter={e => currentSkill(e)} skillname="Django" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/django.svg" /></span>
+            <span className="icon"><img onMouseEnter={e => currentSkill(e)} skillname="PostgreSQL" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/postgresql.svg" /></span>
+            <span className="icon"><img onMouseEnter={e => currentSkill(e)} skillname="Heroku" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/heroku.svg" /></span>
+            <span className="icon"><img onMouseEnter={e => currentSkill(e)} skillname="SQLite" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/sqlite.svg" /></span>
+            <span className="icon"><img onMouseEnter={e => currentSkill(e)} skillname="MongoDB" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/mongodb.svg" /></span>
+            <span className="icon"><img onMouseEnter={e => currentSkill(e)} skillname="Git" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/git.svg" /></span>
+            <span className="icon"><img onMouseEnter={e => currentSkill(e)} skillname="GitHub" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/github.svg" /></span>
+            <span className="icon"><img onMouseEnter={e => currentSkill(e)} skillname="Twilio" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/twilio.svg" /></span>
+            <span className="icon"><img onMouseEnter={e => currentSkill(e)} skillname="Sass" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/sass.svg" /></span>
+            <span className="icon"><img onMouseEnter={e => currentSkill(e)} skillname="Bulma" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/bulma.svg" /></span>
+            <span className="icon"><img onMouseEnter={e => currentSkill(e)} skillname="Material-UI" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/material-ui.svg" /></span>
+            <span className="icon"><img onMouseEnter={e => currentSkill(e)} skillname="Webpack" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/webpack.svg" /></span>
+            <span className="icon"><img onMouseEnter={e => currentSkill(e)} skillname="Babel" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/babel.svg" /></span>
+            <span className="icon"><img onMouseEnter={e => currentSkill(e)} skillname="Bootstrap" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/bootstrap.svg" /></span>
+            <span className="icon"><img onMouseEnter={e => currentSkill(e)} skillname="Recharts/D3.js" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/d3-dot-js.svg" /></span>
+            <span className="icon"><img onMouseEnter={e => currentSkill(e)} skillname="Redux" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/redux.svg" /></span>
+            <span className="icon"><img onMouseEnter={e => currentSkill(e)} skillname="Apollo" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/apollographql.svg" /></span>
+            <span className="icon"><img onMouseEnter={e => currentSkill(e)} skillname="GraphQL" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/graphql.svg" /></span>
+            <span className="icon"><img onMouseEnter={e => currentSkill(e)} skillname="npm" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/npm.svg" /></span>
+            <span className="icon"><img onMouseEnter={e => currentSkill(e)} skillname="Yarn" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/yarn.svg" /></span>
+            <span className="icon"><img onMouseEnter={e => currentSkill(e)} skillname="Mocha" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/mocha.svg" /></span>
+            <span className="icon"><img onMouseEnter={e => currentSkill(e)} skillname="Jest" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/jest.svg" /></span>
+            <span className="icon"><img onMouseEnter={e => currentSkill(e)} skillname="Socket.io" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/socket-dot-io.svg" /></span>
+            <span className="icon"><img onMouseEnter={e => currentSkill(e)} skillname="VSCode" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/visualstudiocode.svg" /></span>
+            <span className="icon"><img onMouseEnter={e => currentSkill(e)} skillname="Postman" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/postman.svg" /></span>
+            <span className="icon"><img onMouseEnter={e => currentSkill(e)} skillname="Insomnia" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/insomnia.svg" /></span>
+            <span className="icon"><img onMouseEnter={e => currentSkill(e)} skillname="StackOverflow" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/stackoverflow.svg" /></span>
+            <span className="icon"><img onMouseEnter={e => currentSkill(e)} skillname="Figma" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/figma.svg" /></span>
           </ul>
         </div>
       </section>
