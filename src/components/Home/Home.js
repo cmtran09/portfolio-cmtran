bc245037e1a0cbae0f6bc8277d97f1c6ecc89830
cmtran09 <cmtran@hotmail.co.uk>
2020-07-01 15:05:27 +0100

    refactored, icons in component, forwardrefs

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -8,15 +8,19 @@ import 'bulma/css/bulma.css'
 // import '../styles/styles.scss'
 
 import ProjectLink from '../ProjectLink/ProjectLink'
+import SkillIcons from '../SkillIcons/SkillIcons'
 
 export default function Home() {
   const [skill, setSkill] = useState('')
   let main = useRef(null)
   let name = useRef(null)
   let content = useRef(null)
+
   let skillIcons = useRef(null)
   let skillSection = useRef(null)
+
   let skillLabel = useRef(null)
+
   let projects = useRef(null)
   let tl = new TimelineLite()
 
@@ -37,12 +41,8 @@ export default function Home() {
     const firstLink = content.children[2].children[1].children[0].children[0]
     const secondLink = firstLink.nextSibling
     const thirdLink = secondLink.nextSibling
-    // skills js var
-    const skillsArr = skillIcons.children
-    console.log('arr', skillIcons.children)
-    const skillJS = skillIcons.firstElementChild
-    const skillhtml5 = skillJS.nextSibling
-    console.log(skillJS)
+    
+    
     //Remove Initial Flash
     TweenMax.to(main, 0, { css: { visibility: 'visible' } })
     // Name animation
@@ -56,24 +56,12 @@ export default function Home() {
       delay: 0.3
     }, 0.3)
 
-    //SKill animation
-    TweenMax.from(skillsArr, 0.07, {
-      scrollTrigger: {
-        trigger: skillSection,
-        start: "top center",
-        markers: true,
-        // scrub: true
-      },
-      x: 80,
-      stagger: 0.1,
-      opacity: 0,
-    }, 0.1);
-
+    
 
     //Projects Var
     const projectsArr = projects.children
     console.log(projectsArr)
-    console.log(typeof(projectsArr))
+    console.log(typeof (projectsArr))
     TweenMax.from(projectsArr[0], 0.07, {
       scrollTrigger: {
         trigger: projectsArr[0],
@@ -87,7 +75,6 @@ export default function Home() {
     }, 0.1);
 
     for (const [key, value] of Object.entries(projectsArr)) {
-      console.log(`OASDOJAOJD${key}: ${value}`);
       TweenMax.from(value.children[0], 0.3, {
         scrollTrigger: {
           trigger: value,
@@ -101,6 +88,30 @@ export default function Home() {
       }, 0.1);
     }
   }, [])
+
+  console.log(skillIcons, 'parenoost')
+
+
+  useEffect(() => {
+    console.log(skillIcons.current, 'parent skillsIcons')
+    // skills js var
+    const skillsArr = skillIcons.current.children
+    console.log('skillsArr', skillsArr)
+    console.log('arr', skillIcons.children)
+
+    //SKill animation
+    TweenMax.from(skillsArr, 0.07, {
+      scrollTrigger: {
+        trigger: skillSection,
+        start: "top center",
+        markers: true,
+        // scrub: true
+      },
+      x: 80,
+      stagger: 0.1,
+      opacity: 0,
+    }, 0.1);
+  }, [skillIcons])
 
   const handleIconMouseEnter = (e) => {
     setSkill(e.target.attributes.getNamedItem("skillname").value)
@@ -155,47 +166,9 @@ export default function Home() {
       </section>
       <section className="section skills-section" ref={elem => skillSection = elem}>
         <div className="container skills-wrapper">
-          <ul ref={elem => skillIcons = elem} className="skills">
-            <span className="icon"><img onMouseLeave={e => handleIconMouseLeave(e)} onMouseEnter={e => handleIconMouseEnter(e)} skillname="JavaScript" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/javascript.svg" /></span>
-            <span className="icon"><img onMouseLeave={e => handleIconMouseLeave(e)} onMouseEnter={e => handleIconMouseEnter(e)} skillname="HTML5" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/html5.svg" /></span>
-            <span className="icon"><img onMouseLeave={e => handleIconMouseLeave(e)} onMouseEnter={e => handleIconMouseEnter(e)} skillname="CSS3" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/css3.svg" /></span>
-            <span className="icon"><img onMouseLeave={e => handleIconMouseLeave(e)} onMouseEnter={e => handleIconMouseEnter(e)} skillname="Node.js" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/node-dot-js.svg" /></span>
-            <span className="icon"><img onMouseLeave={e => handleIconMouseLeave(e)} onMouseEnter={e => handleIconMouseEnter(e)} skillname="Express.js" src="https://www.vectorlogo.zone/logos/expressjs/expressjs-icon.svg"></img></span>
-            <span className="icon"><img onMouseLeave={e => handleIconMouseLeave(e)} onMouseEnter={e => handleIconMouseEnter(e)} skillname="React" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/react.svg" /></span>
-            <span className="icon"><img onMouseLeave={e => handleIconMouseLeave(e)} onMouseEnter={e => handleIconMouseEnter(e)} skillname="React Router" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/reactrouter.svg" /></span>
-            <span className="icon"><img onMouseLeave={e => handleIconMouseLeave(e)} onMouseEnter={e => handleIconMouseEnter(e)} skillname="Python" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/python.svg" /></span>
-            <span className="icon"><img onMouseLeave={e => handleIconMouseLeave(e)} onMouseEnter={e => handleIconMouseEnter(e)} skillname="Django" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/django.svg" /></span>
-            <span className="icon"><img onMouseLeave={e => handleIconMouseLeave(e)} onMouseEnter={e => handleIconMouseEnter(e)} skillname="PostgreSQL" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/postgresql.svg" /></span>
-            <span className="icon"><img onMouseLeave={e => handleIconMouseLeave(e)} onMouseEnter={e => handleIconMouseEnter(e)} skillname="Heroku" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/heroku.svg" /></span>
-            <span className="icon"><img onMouseLeave={e => handleIconMouseLeave(e)} onMouseEnter={e => handleIconMouseEnter(e)} skillname="SQLite" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/sqlite.svg" /></span>
-            <span className="icon"><img onMouseLeave={e => handleIconMouseLeave(e)} onMouseEnter={e => handleIconMouseEnter(e)} skillname="MongoDB" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/mongodb.svg" /></span>
-            <span className="icon"><img onMouseLeave={e => handleIconMouseLeave(e)} onMouseEnter={e => handleIconMouseEnter(e)} skillname="Git" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/git.svg" /></span>
-            <span className="icon"><img onMouseLeave={e => handleIconMouseLeave(e)} onMouseEnter={e => handleIconMouseEnter(e)} skillname="GitHub" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/github.svg" /></span>
-            <span className="icon"><img onMouseLeave={e => handleIconMouseLeave(e)} onMouseEnter={e => handleIconMouseEnter(e)} skillname="Twilio" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/twilio.svg" /></span>
-            <span className="icon"><img onMouseLeave={e => handleIconMouseLeave(e)} onMouseEnter={e => handleIconMouseEnter(e)} skillname="Sass" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/sass.svg" /></span>
-            <span className="icon"><img onMouseLeave={e => handleIconMouseLeave(e)} onMouseEnter={e => handleIconMouseEnter(e)} skillname="Bulma" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/bulma.svg" /></span>
-            <span className="icon"><img onMouseLeave={e => handleIconMouseLeave(e)} onMouseEnter={e => handleIconMouseEnter(e)} skillname="Material-UI" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/material-ui.svg" /></span>
-            <span className="icon"><img onMouseLeave={e => handleIconMouseLeave(e)} onMouseEnter={e => handleIconMouseEnter(e)} skillname="Webpack" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/webpack.svg" /></span>
-            <span className="icon"><img onMouseLeave={e => handleIconMouseLeave(e)} onMouseEnter={e => handleIconMouseEnter(e)} skillname="Babel" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/babel.svg" /></span>
-            <span className="icon"><img onMouseLeave={e => handleIconMouseLeave(e)} onMouseEnter={e => handleIconMouseEnter(e)} skillname="Bootstrap" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/bootstrap.svg" /></span>
-            <span className="icon"><img onMouseLeave={e => handleIconMouseLeave(e)} onMouseEnter={e => handleIconMouseEnter(e)} skillname="Recharts/D3.js" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/d3-dot-js.svg" /></span>
-            <span className="icon"><img onMouseLeave={e => handleIconMouseLeave(e)} onMouseEnter={e => handleIconMouseEnter(e)} skillname="Redux" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/redux.svg" /></span>
-            <span className="icon"><img onMouseLeave={e => handleIconMouseLeave(e)} onMouseEnter={e => handleIconMouseEnter(e)} skillname="Apollo" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/apollographql.svg" /></span>
-            <span className="icon"><img onMouseLeave={e => handleIconMouseLeave(e)} onMouseEnter={e => handleIconMouseEnter(e)} skillname="GraphQL" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/graphql.svg" /></span>
-            <span className="icon"><img onMouseLeave={e => handleIconMouseLeave(e)} onMouseEnter={e => handleIconMouseEnter(e)} skillname="npm" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/npm.svg" /></span>
-            <span className="icon"><img onMouseLeave={e => handleIconMouseLeave(e)} onMouseEnter={e => handleIconMouseEnter(e)} skillname="Yarn" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/yarn.svg" /></span>
-            <span className="icon"><img onMouseLeave={e => handleIconMouseLeave(e)} onMouseEnter={e => handleIconMouseEnter(e)} skillname="Mocha" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/mocha.svg" /></span>
-            <span className="icon"><img onMouseLeave={e => handleIconMouseLeave(e)} onMouseEnter={e => handleIconMouseEnter(e)} skillname="Jest" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/jest.svg" /></span>
-            <span className="icon"><img onMouseLeave={e => handleIconMouseLeave(e)} onMouseEnter={e => handleIconMouseEnter(e)} skillname="Socket.io" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/socket-dot-io.svg" /></span>
-            <span className="icon"><img onMouseLeave={e => handleIconMouseLeave(e)} onMouseEnter={e => handleIconMouseEnter(e)} skillname="VSCode" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/visualstudiocode.svg" /></span>
-            <span className="icon"><img onMouseLeave={e => handleIconMouseLeave(e)} onMouseEnter={e => handleIconMouseEnter(e)} skillname="Postman" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/postman.svg" /></span>
-            <span className="icon"><img onMouseLeave={e => handleIconMouseLeave(e)} onMouseEnter={e => handleIconMouseEnter(e)} skillname="Insomnia" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/insomnia.svg" /></span>
-            <span className="icon"><img onMouseLeave={e => handleIconMouseLeave(e)} onMouseEnter={e => handleIconMouseEnter(e)} skillname="StackOverflow" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/stackoverflow.svg" /></span>
-            <span className="icon"><img onMouseLeave={e => handleIconMouseLeave(e)} onMouseEnter={e => handleIconMouseEnter(e)} skillname="Figma" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/figma.svg" /></span>
-            <span className="icon"><img onMouseLeave={e => handleIconMouseLeave(e)} onMouseEnter={e => handleIconMouseEnter(e)} skillname="Greensock" src="https://cdn.jsdelivr.net/npm/simple-icons@v2/icons/greensock.svg" /></span>
-          </ul>
+          <SkillIcons skillIconsRef={skillIcons} handleIconMouseLeave={handleIconMouseLeave} handleIconMouseEnter={handleIconMouseEnter} />
         </div>
-      </section>
+      </section >
       <section className="section" ref={elem => projects = elem}>
         <ProjectLink projectName="Swell" projectLink="swell.com" projectRepo="git.swell" />
         <ProjectLink projectName="Swell" projectLink="swell.com" projectRepo="git.swell" />
@@ -214,6 +187,6 @@ export default function Home() {
           </p>
         </div>
       </section>
-    </main>
+    </main >
   )
 }
--- src/components/ProjectLink/ProjectLink.js
+++ src/components/ProjectLink/ProjectLink.js
@@ -1,7 +1,6 @@
 import React from 'react'
 
 export default function ProjectLink(props) {
-  console.log('props',props)
   return (
     <div className="container project-wrapper">
       <p className="project-name">{props.projectName}</p>
