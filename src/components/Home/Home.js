c0886f56566f5dcb855b61daeb86c8204efa421f
cmtran09 <cmtran@hotmail.co.uk>
2020-07-01 17:18:12 +0100

    project links timeline addeed

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -24,15 +24,6 @@ export default function Home() {
   let projects = useRef(null)
   let tl = new TimelineLite()
 
-  // useEffect(() => {
-  //   let tlSkill = new TimelineLite()
-  //   const skillLabelText = skillLabel.firstElementChild
-  //   console.log(skillLabelText)
-  //   tlSkill
-  //     .from(skillLabelText, 3, { x: "50vh", opacity: 1 })
-  //     .to(skillLabelText, 2, { opacity: 0, delay: 4 })
-  // }, [skill])
-
   useEffect(() => {
     //Name var
     const myName = name.firstElementChild
@@ -41,8 +32,8 @@ export default function Home() {
     const firstLink = content.children[2].children[1].children[0].children[0]
     const secondLink = firstLink.nextSibling
     const thirdLink = secondLink.nextSibling
-    
-    
+
+
     //Remove Initial Flash
     TweenMax.to(main, 0, { css: { visibility: 'visible' } })
     // Name animation
@@ -56,48 +47,11 @@ export default function Home() {
       delay: 0.3
     }, 0.3)
 
-    
-
-    //Projects Var
-    const projectsArr = projects.children
-    console.log(projectsArr)
-    console.log(typeof (projectsArr))
-    TweenMax.from(projectsArr[0], 0.07, {
-      scrollTrigger: {
-        trigger: projectsArr[0],
-        start: "top center",
-        markers: true,
-        // scrub: true
-      },
-      x: 80,
-      stagger: 0.1,
-      opacity: 0,
-    }, 0.1);
-
-    for (const [key, value] of Object.entries(projectsArr)) {
-      TweenMax.from(value.children[0], 0.3, {
-        scrollTrigger: {
-          trigger: value,
-          start: "top center",
-          markers: true,
-          // scrub: true
-        },
-        y: -80,
-        stagger: 0.1,
-        opacity: 0,
-      }, 0.1);
-    }
-  }, [])
-
-  console.log(skillIcons, 'parenoost')
-
-
-  useEffect(() => {
     console.log(skillIcons.current, 'parent skillsIcons')
     // skills js var
     const skillsArr = skillIcons.current.children
-    console.log('skillsArr', skillsArr)
-    console.log('arr', skillIcons.children)
+    // console.log('skillsArr', skillsArr)
+    // console.log('arr', skillIcons.children)
 
     //SKill animation
     TweenMax.from(skillsArr, 0.07, {
@@ -111,82 +65,119 @@ export default function Home() {
       stagger: 0.1,
       opacity: 0,
     }, 0.1);
-  }, [skillIcons])
 
-  const handleIconMouseEnter = (e) => {
-    setSkill(e.target.attributes.getNamedItem("skillname").value)
-    skillLabel.classList.add('skills-animation')
+    //Projects Var
+    const projectsArr = projects.children
+    console.log(projectsArr)
+    console.log(typeof (projectsArr))
+    // TweenMax.from(projectsArr[0], 0.07, {
+    //   scrollTrigger: {
+    //     trigger: projectsArr[0],
+    //     start: "top center",
+    //     markers: true,
+    //     // scrub: true
+    //   },
+    //   x: 80,
+    //   stagger: 0.1,
+    //   opacity: 0,
+    // }, 0.1);
+
+    for (const [key, value] of Object.entries(projectsArr)) {
+      TweenMax.from(value.children[0], 0.3, {
+        scrollTrigger: {
+          trigger: value,
+          start: "top center",
+          markers: true,
+          // scrub: true
+        },
+        y: "-5vw",
+        stagger: 0.1,
+        opacity: 0,
+      }, 0.1);
+
+      console.log(value.children, 'value object')
+
+    }
+  }, [])
+
+  const handleProjectHover = (e) => {
+    console.log('hi')
   }
 
-  const handleIconMouseLeave = (e) => {
-    skillLabel.classList.remove('skills-animation')
-  }
+const handleIconMouseEnter = (e) => {
+  setSkill(e.target.attributes.getNamedItem("skillname").value)
+  skillLabel.classList.add('skills-animation')
+}
 
-  return (
-    <main ref={elem => main = elem} >
-      <div className="skill-namer" ref={elem => skillLabel = elem}>
-        <p className="skill-text">{skill ? skill : ""}</p>
-      </div>
-      <section className="section">
-        <div className="container main-wrapper" ref={elem => content = elem}>
-          <div className="container header-wrapper">
-            <div className="level header is-mobile">
-              <p className="level-left">Portfolio</p>
-              <p className="level-item">2020</p>
-              <p className="level-right">London</p>
-            </div>
+const handleIconMouseLeave = (e) => {
+  skillLabel.classList.remove('skills-animation')
+}
+
+return (
+  <main ref={elem => main = elem} >
+    <div className="skill-namer" ref={elem => skillLabel = elem}>
+      <p className="skill-text">{skill ? skill : ""}</p>
+    </div>
+    <section className="section">
+      <div className="container main-wrapper" ref={elem => content = elem}>
+        <div className="container header-wrapper">
+          <div className="level header is-mobile">
+            <p className="level-left">Portfolio</p>
+            <p className="level-item">2020</p>
+            <p className="level-right">London</p>
           </div>
-          <div className="name-container" ref={elem => name = elem}>
-            <h1 className="name">
-              Cuong Tran
+        </div>
+        <div className="name-container" ref={elem => name = elem}>
+          <h1 className="name">
+            Cuong Tran
             </h1>
-          </div>
-          <div className="columns main-column is-mobile hero-content">
-            <div className="column is-three-quarters">
-              <p className="about">
-                I'm a Software Engineer from London, recently graduated at General Assembly.
-                Here are some pieces of my work. Hope you enjoy having a look.
+        </div>
+        <div className="columns main-column is-mobile hero-content">
+          <div className="column is-three-quarters">
+            <p className="about">
+              I'm a Software Engineer from London, recently graduated at General Assembly.
+              Here are some pieces of my work. Hope you enjoy having a look.
               </p>
-            </div>
-            <div className="column">
-              <ul className="links">
-                <div className="link-container">
-                  <li>GitHub</li>
-                </div>
-                <div className="link-container">
-                  <li>LinkedIn</li>
-                </div>
-                <div className="link-container">
-                  <li>CodeWars</li>
-                </div>
-              </ul>
-            </div>
+          </div>
+          <div className="column">
+            <ul className="links">
+              <div className="link-container">
+                <li>GitHub</li>
+              </div>
+              <div className="link-container">
+                <li>LinkedIn</li>
+              </div>
+              <div className="link-container">
+                <li>CodeWars</li>
+              </div>
+            </ul>
           </div>
         </div>
-      </section>
-      <section className="section skills-section" ref={elem => skillSection = elem}>
-        <div className="container skills-wrapper">
-          <SkillIcons skillIconsRef={skillIcons} handleIconMouseLeave={handleIconMouseLeave} handleIconMouseEnter={handleIconMouseEnter} />
-        </div>
-      </section >
-      <section className="section" ref={elem => projects = elem}>
-        <ProjectLink projectName="Swell" projectLink="swell.com" projectRepo="git.swell" />
-        <ProjectLink projectName="Swell" projectLink="swell.com" projectRepo="git.swell" />
-        <ProjectLink projectName="Swell" projectLink="swell.com" projectRepo="git.swell" />
-        <ProjectLink projectName="Swell" projectLink="swell.com" projectRepo="git.swell" />
-        <ProjectLink projectName="Swell" projectLink="swell.com" projectRepo="git.swell" />
-        <ProjectLink projectName="Swell" projectLink="swell.com" projectRepo="git.swell" />
-        <ProjectLink projectName="Swell" projectLink="swell.com" projectRepo="git.swell" />
-        <ProjectLink projectName="Swell" projectLink="swell.com" projectRepo="git.swell" />
-      </section>
-      <section className="section">
-        <div className="container contact-wrapper">
-          <h1 className="contact">Contact</h1>
-          <p className="about">
-            I am currently looking for a Junior Software Engineer/Front-End/Fullstack role in London. Feel free to drop me a LinkedIn message or email at cuomantran@gmail.com.
+      </div>
+    </section>
+    <section className="section skills-section" ref={elem => skillSection = elem}>
+      <div className="container skills-wrapper">
+        <SkillIcons skillIconsRef={skillIcons} handleIconMouseLeave={handleIconMouseLeave} handleIconMouseEnter={handleIconMouseEnter} />
+      </div>
+    </section >
+    <section className="section" ref={elem => projects = elem}>
+      <ProjectLink projectName="Swell" projectLink="swell.com" projectRepo="git.swell" handleProjectHover={handleProjectHover}/>
+      <ProjectLink projectName="Swell" projectLink="swell.com" projectRepo="git.swell" handleProjectHover={handleProjectHover}/>
+      <ProjectLink projectName="Swell" projectLink="swell.com" projectRepo="git.swell" handleProjectHover={handleProjectHover}/>
+      <ProjectLink projectName="Swell" projectLink="swell.com" projectRepo="git.swell" handleProjectHover={handleProjectHover}/>
+      <ProjectLink projectName="Swell" projectLink="swell.com" projectRepo="git.swell" handleProjectHover={handleProjectHover}/>
+      <ProjectLink projectName="Swell" projectLink="swell.com" projectRepo="git.swell" handleProjectHover={handleProjectHover}/>
+      <ProjectLink projectName="Swell" projectLink="swell.com" projectRepo="git.swell" handleProjectHover={handleProjectHover}/>
+      <ProjectLink projectName="Swell" projectLink="swell.com" projectRepo="git.swell" handleProjectHover={handleProjectHover}/>
+    </section>
+    <section className="section">
+      <div className="container contact-wrapper">
+        <h1 className="contact">Contact</h1>
+        <p className="about">
+          I am currently looking for a Junior Software Engineer/Front-End/Fullstack role in London. Feel free to drop me a LinkedIn message or email at cuomantran@gmail.com.
           </p>
-        </div>
-      </section>
-    </main >
-  )
+      </div>
+    </section>
+  </main >
+)
 }
--- src/components/ProjectLink/ProjectLink.js
+++ src/components/ProjectLink/ProjectLink.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
+import { TimelineLite, Power3 } from 'gsap'
 
-export default function ProjectLink(props) {
+export default function ProjectLink({ projectName, projectLink, projectRepo }) {
+  let githubLink = useRef(null)
+  let deployedLink = useRef(null)
+  const tl = new TimelineLite()
+
+  useEffect(() => {
+    tl.from(deployedLink, { y: 10, opacity: 0, ease: Power3.easeOut })
+      .from(githubLink, { y: 10, opacity: 0, ease: Power3.easeOut }, 0.3)
+    tl.reversed(true)
+  }, [])
+
+  const projectAnimation = () => {
+    tl.reversed(!tl.reversed())
+  }
   return (
-    <div className="container project-wrapper">
-      <p className="project-name">{props.projectName}</p>
+    <div className="container project-wrapper" onMouseOver={() => projectAnimation()} onMouseLeave={() => projectAnimation()}>
+      <p className="project-name">{projectName}</p>
+      <div ref={elem => deployedLink = elem} className="deployed-site">deployed</div>
+      <div ref={elem => githubLink = elem} className="github-repo">github</div>
     </div>
   )
 }
