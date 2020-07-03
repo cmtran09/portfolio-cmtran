7f68f029de16688acfacb9cfdd6e5312241adc76
cmtran09 <cmtran@hotmail.co.uk>
2020-07-03 20:55:07 +0100

    react-player added theatermode on off

--- src/components/ProjectLink/ProjectLink.js
+++ src/components/ProjectLink/ProjectLink.js
@@ -1,22 +1,24 @@
-import React, { useRef, useEffect, useState } from 'react'
+import React, { useRef, useEffect, useState, useMemo } from 'react'
 import { TimelineLite, Power3 } from 'gsap'
 import ReactPlayer from 'react-player'
 
 export default function ProjectLink({ projectName, projectLink, projectRepo, projectImage, projectVideo }) {
+  const [demoIsActive, setDemoIsActive] = useState(false)
   let githubLink = useRef(null)
   let deployedLink = useRef(null)
   let demoPlay = useRef(null)
   let projectContainer = useRef(null)
   let videoContainer = useRef(null)
-  const tl = new TimelineLite()
 
-  const [demoIsActive, setDemoIsActive] = useState(false)
+  const tl = useMemo(() => new TimelineLite(), [])
+
 
   useEffect(() => {
-    tl.from(deployedLink, { y: 10, opacity: 0, ease: Power3.easeOut })
-      .from(githubLink, { y: 10, opacity: 0, ease: Power3.easeOut }, 0.3)
-      .from(demoPlay, { y: 10, opacity: 0, ease: Power3.easeOut }, 0.6)
-    tl.reversed(true)
+    tl
+      .to(demoPlay, { y: 10, opacity: 0, ease: Power3.easeOut })
+      .to(githubLink, { y: 10, opacity: 0, ease: Power3.easeOut }, 0.2)
+      .to(deployedLink, { y: 10, opacity: 0, ease: Power3.easeOut }, 0.4)
+    // tl.reversed(true)
     // console.log(projectContainer, 'projectContainer inside'
   }, [])
 
@@ -27,30 +29,49 @@ export default function ProjectLink({ projectName, projectLink, projectRepo, pro
       tl.reversed(!tl.reversed())
     } return
   }
+
+  const handleMouseLeave = () => {
+    if (!demoIsActive) return tl.reversed(!tl.reversed())
+    // return tl.paused()
+  }
+
   return (
     <div style={{
       position: 'relative'
-    }} ref={elem => projectContainer = elem} className="container project-wrapper hidden" onMouseEnter={() => projectAnimation()} onMouseLeave={() => projectAnimation()}>
+    }} ref={elem => projectContainer = elem} className="container project-wrapper hidden" onMouseEnter={() => projectAnimation()} onMouseLeave={() => handleMouseLeave()}>
       <p className="project-name">{projectName}</p>
       <div ref={elem => deployedLink = elem} className="deployed-site">deployed</div>
       <div ref={elem => githubLink = elem} className="github-repo">github</div>
-      <div ref={elem => demoPlay = elem} onClick={() => setDemoIsActive(!demoIsActive)} className="github-repo">demo</div>
+      <div
+        ref={elem => demoPlay = elem}
+        onClick={() => {
+          setDemoIsActive(!demoIsActive)
+          !demoIsActive ? tl.pause() : tl.resume()
+          // tl.reversed(false)
+        }}
+        className="demo-link">demo
+      </div>
       {demoIsActive &&
-        <div className="demo-wrapper" onClick={() => setDemoIsActive(!demoIsActive)} style={{
-          position: "fixed",
-          top: "0",
-          left: "0",
-          width: "100%",
-          height: "100%",
-          zIndex: "9",
-          background: "rgba(0, 0, 0, 0.5)"
-        }}>
+        <div
+          className="demo-wrapper"
+          onClick={() => {
+            setDemoIsActive(!demoIsActive)
+            tl.resume()
+          }}
+          style={{
+            position: "fixed",
+            top: "0",
+            left: "0",
+            width: "100%",
+            height: "100%",
+            zIndex: "9",
+            background: "rgba(0, 0, 0, 0.5)"
+          }}>
           <div style={{
             position: "absolute",
             left: "50%",
             transform: "translateX(-50%)",
-            top: "25vh"
-            // transform: "translateY(-50%)",
+            top: "30vh"
           }}>
             <ReactPlayer url={projectVideo} playing loop />
           </div>
