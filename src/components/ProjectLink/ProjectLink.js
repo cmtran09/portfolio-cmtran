b840f90703df50a344a1b4db017211218d26129a
cmtran09 <cmtran@hotmail.co.uk>
2020-07-03 18:58:41 +0100

    react-player added theatermode on off

--- src/components/ProjectLink/ProjectLink.js
+++ src/components/ProjectLink/ProjectLink.js
@@ -35,7 +35,27 @@ export default function ProjectLink({ projectName, projectLink, projectRepo, pro
       <div ref={elem => deployedLink = elem} className="deployed-site">deployed</div>
       <div ref={elem => githubLink = elem} className="github-repo">github</div>
       <div ref={elem => demoPlay = elem} onClick={() => setDemoIsActive(!demoIsActive)} className="github-repo">demo</div>
-      {demoIsActive && <ReactPlayer url={projectVideo} playing/>}
+      {demoIsActive &&
+        <div className="demo-wrapper" onClick={() => setDemoIsActive(!demoIsActive)} style={{
+          position: "fixed",
+          top: "0",
+          left: "0",
+          width: "100%",
+          height: "100%",
+          zIndex: "9",
+          background: "rgba(0, 0, 0, 0.5)"
+        }}>
+          <div style={{
+            position: "absolute",
+            left: "50%",
+            transform: "translateX(-50%)",
+            top: "25vh"
+            // transform: "translateY(-50%)",
+          }}>
+            <ReactPlayer url={projectVideo} playing loop />
+          </div>
+        </div>
+      }
       {/* <img src={projectImage} alt=""/> */}
       {/* <div ref={elem => videoContainer = elem} className="video-container"
         style={{
@@ -50,6 +70,6 @@ export default function ProjectLink({ projectName, projectLink, projectRepo, pro
           <source src={projectVideo} type="video/mp4" />
         </video>
       </div> */}
-    </div>
+    </div >
   )
 }
